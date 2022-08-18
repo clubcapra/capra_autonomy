4ca46f999d292a53a24ff8de1289367c5984cee2
kcrackhead <firechops420@hotmail.com>
2022-08-18 16:05:21 -0400

    added logic to IK

--- scripts/InverseKinematic.py
+++ scripts/InverseKinematic.py
@@ -1,57 +1,12 @@
-#!/usr/bin/env python
-import rospy
-from geometry_msgs.msg import Twist
-#max linear speed = 1.5 m/s
-#max angular speed = 4 rad/s
-LINEAR_SPEED=1.0
-ANGULAR_SPEED=1.0
+import math
+x = 1000
+y = 1000
+theta = tan^-1(x/y)
+distance = (x^2+y^2)^(1/2)
 
-move = Twist() # defining the way we can allocate the values
+#tourner robot de theta
+#avancer de distance
 
+#OU
 
-def kinematic() :
-
-    move.linear.x = 1.0 # allocating the values in x direction - linear
-    move.angular.z = 1.0  # allocating the values in z direction - angular
-    move_pub = rospy.Publisher('/turtle1/cmd_vel', move, queue_size=1) 
-    move_pub.publish()
-
-    return
-
-
-def arreter() :
-
-    move.linear.x = 0.0 # allocating the values in x direction - linear
-    move.angular.z = 0.0  # allocating the values in z direction - angular
-    kinematic()
-
-    return
-
-
-def bouger(Vx,Rz,t) :
-
-    move.linear.x = 1.0 # allocating the values in x direction - linear
-    move.angular.z = 1.0  # allocating the values in z direction - angular
-    kinematic()
-    rospy.sleep(t)
-    arreter()
-
-    return
-
-
-def avancer(t) :
-
-    bouger(LINEAR_SPEED,0,t)
-    rospy.sleep(t)
-    arreter()
-
-    return
-
-
-def tourner(t) :
-    
-    bouger(0,ANGULAR_SPEED,t)
-    rospy.sleep(t)
-    arreter()
-
-    return
+#rayon de cercle
